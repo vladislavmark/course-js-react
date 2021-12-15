db5b9842ba57381b0f1912601c11fe97180e070f
mark <vladys.marchenko@gmail.com>
2021-12-15 12:42:30 +0200

    cycles

---
+++ 014/index.js
@@ -0,0 +1,20 @@
+let num = 50;
+
+while (num < 55) {
+    console.log(num);
+    num++;
+}
+
+do {
+    console.log(num);
+    num++;
+}
+while (num < 55);
+
+for (let i = 0; i < 10; i++) {
+    if (i === 6) {
+        // break;
+        continue;
+    }
+    console.log(i);
+}
