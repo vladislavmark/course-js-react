436b782293d7e98801dbd832b7f6a9a024eae96b
mark <vladys.marchenko@gmail.com>
2021-12-15 11:49:47 +0200

    условие

---
+++ 013/index.js
@@ -0,0 +1,20 @@
+if (4 == 5) {
+    console.log('ok');
+} else {
+    console.log('no');
+}
+
+const num = 43;
+(num === 50) ? console.log('ok') : console.log('err');
+
+switch (num) {
+    case 49:
+        console.log('no');
+        break;
+    case 40:
+        console.log('yes');
+        break;
+    default:
+        console.log('nono');
+        break;
+}
