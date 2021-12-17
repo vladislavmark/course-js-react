ec10b6b7c211ddfe38cacee1355f9e6e559bd2bb
mark <vladys.marchenko@gmail.com>
2021-12-17 10:37:29 +0200

    function

---
+++ 016/index.js
@@ -0,0 +1,16 @@
+let num = 20;
+
+function showMes(params) {
+    console.log(params);
+    let num = 10;
+}
+
+showMes('text test');
+
+let logger = function () {
+    console.log('function exspression');
+};
+
+logger();
+
+let newTestFunction = (a, b) => a + b;
