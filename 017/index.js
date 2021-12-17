c0ac4f6025d23a7d37fc574bbf2cc14e367a3a71
mark <vladys.marchenko@gmail.com>
2021-12-17 12:06:17 +0200

    methods number and string

---
+++ 017/index.js
@@ -0,0 +1,15 @@
+const str = 'test';
+console.log(str.length);
+
+const fruit = "some fruit";
+console.log(fruit.indexOf('f'));
+
+const logg = "hello world";
+console.log(logg.slice(5));
+console.log(logg.substr(5, 7));
+
+const num = 5.6;
+console.log(Math.round(num));
+
+const test = "12.6px";
+console.log(parseInt(test));
