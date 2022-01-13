9616e9ac6ae6c81b76aaf7e0e288cd3f67785012
mark <vladys.marchenko@gmail.com>
2022-01-13 09:33:27 +0200

    DOM navigation

---
+++ 032/script.js
@@ -0,0 +1,21 @@
+// console.log(document.body);
+// console.log(document.documentElement);
+// console.log(document.body.childNodes); // получить все nody родителя 
+// console.log(document.body.firstChild); // получить 1 елемент 
+// console.log(document.body.firstElementChild); // получить 1 елемент 
+// console.log(document.body.lastChild); // получить последний елемент 
+// console.log(document.body.lastElementChild); // получить последний елемент 
+
+// console.log(document.querySelector('#current').parentNode); //получить родителя
+// console.log(document.querySelector('#current').parentElement); //получить родителя
+
+// console.log(document.querySelector('[data-current="3"]').nextElementSibling); //получить data atribute & next element
+// console.log(document.querySelector('[data-current="3"]').previousElementSibling); //получить data atribute & prev eleme
+
+for (let node of document.body.childNodes) { //перебрать все елементы убирая #text
+    if (node.nodeName == '#text') {
+        continue;
+    }
+
+    console.log(node);
+}
