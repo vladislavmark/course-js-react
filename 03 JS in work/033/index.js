56977941d1998efbbc03b90f1a1edd63002ce939
mark <vladys.marchenko@gmail.com>
2022-01-18 16:48:15 +0200

    classlist

---
+++ 03 JS in work/033/index.js
@@ -0,0 +1,21 @@
+const btns = document.querySelectorAll('button');
+
+// console.log(btns[0].classList.length);
+// console.log(btns[0].classList.item(1));
+// console.log(btns[1].classList.add('red'));
+// console.log(btns[0].classList.remove('blue'));
+// console.log(btns[0].classList.toggle('blue'));
+
+// if (btns[1].classList.contains('red')) {
+//     console.log('red');
+// }
+
+btns[0].addEventListener('click', () => {
+    // if (!btns[1].classList.contains('red')) {
+    //     btns[1].classList.add('red');
+    // } else {
+    //     btns[1].classList.remove('red');
+    // }
+    
+    btns[1].classList.toggle('red');
+});
