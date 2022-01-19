663e61c64b5e930522b4a35c3b239a56e9da01cb
mark <vladys.marchenko@gmail.com>
2022-01-19 11:06:57 +0200

    ClassList and Event Delegation

--- 03 JS in work/033/index.js
+++ 03 JS in work/033/index.js
@@ -1,4 +1,5 @@
-const btns = document.querySelectorAll('button');
+const btns = document.querySelectorAll('button'),
+    wrapper = document.querySelector('.btn-block');
 
 // console.log(btns[0].classList.length);
 // console.log(btns[0].classList.item(1));
@@ -11,11 +12,40 @@ const btns = document.querySelectorAll('button');
 // }
 
 btns[0].addEventListener('click', () => {
-    // if (!btns[1].classList.contains('red')) {
-    //     btns[1].classList.add('red');
-    // } else {
-    //     btns[1].classList.remove('red');
-    // }
-    
-    btns[1].classList.toggle('red');
+    if (!btns[1].classList.contains('red')) {
+        btns[1].classList.add('red');
+    } else {
+        btns[1].classList.remove('red');
+    }
+
+    // btns[1].classList.toggle('red');
 });
+
+// wrapper.addEventListener('click', (event) => {
+//     if (event.target && event.target.classList.contains('blue')) {
+//         console.log('acces');
+//     }
+// });
+
+// wrapper.addEventListener('click', (event) => {
+//     if (event.target && event.target.tagName == "BUTTON") {
+//         console.log('acces');
+//     }
+// });
+
+wrapper.addEventListener('click', (event) => {
+    if (event.target && event.target.matches('button.red')) {
+        console.log('acces');
+    }
+});
+
+// без делегирования 
+// btns.forEach(btn => {
+//     btn.addEventListener('click', () => {
+//         console.log('acces');
+//     });
+// });
+
+const btn = document.createElement('button');
+btn.classList.add('red');
+wrapper.append(btn);
