cf78774671157b5080dfb29eb4d1195ec8ad06b4
mark <vladys.marchenko@gmail.com>
2022-06-22 19:31:47 +0300

    js

--- 02 basic script/007/index.js
+++ 02 basic script/007/index.js
@@ -1,3 +1,11 @@
+//007 Простое общение с пользователем
+
+// const result = confirm('are you here');
+// console.log(result);
+
+// const result = +prompt('you have 18 years?', '18');
+// console.log(result + 5);
+
 const answers = [];
 answers[0] = prompt('Question1', '');
 answers[1] = prompt('Question2', '');
